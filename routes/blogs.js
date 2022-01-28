c92cad4fcb44d58ddaa45b5c84cba0e6daa748f5
tbenjamin1 <tbenpollyl@gmail.com>
2022-01-28 15:05:30 +0200

    integration

--- routes/blogs.js
+++ routes/blogs.js
@@ -107,7 +107,7 @@ router.put("/update/:id",auth, async (req, res) => {
 
 });
 
-router.delete("/delete/:id", auth, async (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
 
 
   const blog = await Blog.findByIdAndRemove(req.params.id);
